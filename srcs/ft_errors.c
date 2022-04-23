f749bb746eda6eb195b748ce73e35c62005f88dd
Lucas De alencastro moreira <lde-alen@student.42abudhabi.ae>
2022-04-23 21:43:49 +0400

    DCLL 1 - 4 Lucas (ongoing)

--- srcs/ft_errors.c
+++ srcs/ft_errors.c
@@ -6,7 +6,7 @@
 /*   By: lde-alen <lde-alen@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/19 21:36:11 by lde-alen          #+#    #+#             */
-/*   Updated: 2022/04/23 17:11:48 by lde-alen         ###   ########.fr       */
+/*   Updated: 2022/04/23 19:11:21 by lde-alen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,16 +47,16 @@ void	ft_check_errors(int ac, char **av)
 				if (!ft_isdigit(av[j][i]))
 				{
 					if (av[j][i] != ' ' && av[j][i] != '-')
-						ft_fputstr("Error.\n1");
+						ft_fputstr("Error1.\n");
 					else if (av[j][i] == '-')
 					{
 						if (av[j][i + 1] == 0 || av[j][i + 1] == ' ')
-							ft_fputstr("Error.\n2");
+							ft_fputstr("Error2.\n");
 						else if (ft_isdigit(av[j][i + 1]))
 						{
 							if (i > 0 && (ft_isdigit(av[j][i - 1])
 								|| av[j][i - 1] == '-'))
-								ft_fputstr("Error.\n3");
+								ft_fputstr("Error3.\n");
 						}
 					}
 				}
